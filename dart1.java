c4650b2b2d605a528fe89acff4619ca29b49e3fc
Lme20 <70916382+Lme20@users.noreply.github.com>
2020-09-07 22:07:09 +0200

    Update dart1.java
    lols

--- dart1.java
+++ dart1.java
@@ -2,7 +2,11 @@ public class dart1 {
     public static void main(String[] args){
     
 
-    System.out.println("Hi guys!")
+    System.out.println("Hi guys!");
+    System.out.println("Hello there ;) "):
+
+        
+     
     
 
     }
