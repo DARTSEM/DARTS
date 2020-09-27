7e7e72101f9402c5188f3bacd8c98729e4b7bd04
Lme20 <70916382+Lme20@users.noreply.github.com>
2020-09-27 14:34:36 +0200

    Update DartReturnProductView.java

--- Dart 2/src/com/julia/dart/views/DartReturnProductView.java
+++ Dart 2/src/com/julia/dart/views/DartReturnProductView.java
@@ -1,4 +1,36 @@
 package com.julia.dart.views;
 
+import com.julia.dart.Utilities;
+import java.util.UUID;
+
 public class DartReturnProductView {
+  
+  public void render() {
+        //Costumer view for returning a product -Louis
+        System.out.println(Utilities.line() + "Customer Screen - Type one of the options below:\n" +
+                "1. Enter id of product you wish to return:\n" +
+                "2. Return to Main Menu\n");
+    }
+
+    public void renderExit() {
+        System.out.println("Returning to main menu.");
+    }
+
+    public void renderError() {
+        System.out.println("Invalid id, restarting...");
+    }
+
+    public UUID read() {
+        String input = Utilities.stringInput();
+        UUID retVal = null; //initialize as null otherwise compiler doesn't know what to do with it/ have to give it
+
+        //initial value
+        try {
+            retVal = UUID.fromString(input); //converts UUID from a string
+            return retVal;
+        } catch (IllegalArgumentException e) {
+            return retVal;
+        }
+
+    }
 }
