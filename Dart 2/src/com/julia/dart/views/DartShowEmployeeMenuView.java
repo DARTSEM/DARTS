6b3f91bcc8c1182f68ea58ae5c36cf291ef81425
Ed <70916334+JollerMcAwesome@users.noreply.github.com>
2020-09-27 15:32:28 +0200

    It now shows all employees

--- Dart 2/src/com/julia/dart/views/DartShowEmployeeMenuView.java
+++ Dart 2/src/com/julia/dart/views/DartShowEmployeeMenuView.java
@@ -1,14 +1,23 @@
 package com.julia.dart.views;
 
 import com.julia.dart.Utilities;
+import com.julia.dart.model.*;
+
+import java.util.Collection;
 
 public class DartShowEmployeeMenuView {
-    public void render(){
+    public void render(Collection<DartEmployee> employeeList){
             System.out.println(Utilities.line() + "Employee Database - Type one of the options below:\n" +
                     "1. Search employee\n" +
-                    "2. Remove employee\n");
-    }
+                    "2. Remove employee\n" +
+                    "3. Return to previous menu\n");
+        System.out.println(Utilities.line());
 
+        for (DartEmployee employee : employeeList) {
+            System.out.println(employee);
+        }
+
+    }
     public void renderExit() { System.out.println("Returning to previous menu."); }
 
     public void renderError() { System.out.println("Invalid selection, restarting..."); }
@@ -20,4 +29,3 @@ public class DartShowEmployeeMenuView {
     }
 
 }
-
