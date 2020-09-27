677bb6c1f8bd3717067f9c6e793c3baea69d5cff
Lme20 <70916382+Lme20@users.noreply.github.com>
2020-09-27 16:53:00 +0200

    Update DartRentProductView.java

--- Dart 2/src/com/julia/dart/views/DartRentProductView.java
+++ Dart 2/src/com/julia/dart/views/DartRentProductView.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 public class DartRentProductView {
   
     public void render() {
-        //Costumer view for renting a product -Louis
+        //customer view for renting a product -Louis
         System.out.println(Utilities.line() + "Customer Screen - Type one of the options below:\n" +
                 "1. Enter id of product you wish to rent:\n" +
                 "2. Return to Main Menu\n");
