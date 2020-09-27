70dd55df6e50a01e501ff2b9a3edc75bbb7534e0
Ed <70916334+JollerMcAwesome@users.noreply.github.com>
2020-09-27 16:29:57 +0200

    Update DartShowGamesView.java

--- Dart 2/src/com/julia/dart/views/DartShowGamesView.java
+++ Dart 2/src/com/julia/dart/views/DartShowGamesView.java
@@ -1,12 +1,20 @@
 package com.julia.dart.views;
 
 import com.julia.dart.Utilities;
+import com.julia.dart.model.DartEmployee;
+import com.julia.dart.model.DartProduct;
+
+import java.util.Collection;
 
 public class DartShowGamesView {
-    public void render(){
+    public void render(Collection<DartProduct> productList){
         System.out.println(Utilities.line() + "Product Database - Type one of the options below:\n" +
-                "1. Search product\n" +
-                "2. Remove product\n");
+                "1. Remove product\n" +
+                "2. Return to previous menu\n");
+
+        for (DartProduct product : productList) {
+            System.out.println(product);
+        }
     }
 
     public void renderExit() { System.out.println("Returning to previous menu."); }
